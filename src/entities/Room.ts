fbad06c18d5cb2344dfe3d572e9601b7348467d5
jrrodrigo421 <rjr89000@gmail.com>
2023-08-27 15:51:06 -0300

    create entitie Subject

--- src/entities/Room.ts
+++ src/entities/Room.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Subject } from "./Subject";
 import { Video } from "./Video";
 
 @Entity('rooms')
@@ -14,4 +15,7 @@ export class Room {
 
   @OneToMany(() => Video, video => video.room)
   videos: Video[]
+
+  @ManyToMany(() => Subject, subject => subject.rooms)
+  subjects: Subject[]
 }
