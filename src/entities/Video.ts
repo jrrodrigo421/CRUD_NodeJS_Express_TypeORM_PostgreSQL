39bbcabbe5ab9e78ea16f690872bc46a45bc1568
jrrodrigo421 <rjr89000@gmail.com>
2023-08-27 14:49:53 -0300

    criando entidadese relacionametos

---
+++ src/entities/Video.ts
@@ -0,0 +1,20 @@
+import { StringifyOptions } from "querystring";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Room } from "./Room";
+
+@Entity('videos')
+export class Video {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'text' })
+  title: string
+
+  @Column({ type: 'text' })
+  url: string
+
+  @ManyToOne(() => Room, room => room.videos)
+  @JoinColumn({ name: 'room_id' })
+  room: Room
+
+}
